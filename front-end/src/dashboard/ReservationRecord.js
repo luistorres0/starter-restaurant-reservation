2e5fc895dbfa1d3c73788961360a7d64edc3420e
Luis Torres <luisdtor@gmail.com>
2021-05-17 16:21:39 -0500

    Added a 'Seat' button to each reservation on the dashboard

--- front-end/src/dashboard/ReservationRecord.js
+++ front-end/src/dashboard/ReservationRecord.js
@@ -1,15 +1,17 @@
 import React from "react";
 
 const ReservationRecord = (props) => {
-  const { first_name, last_name, mobile_number, reservation_date, reservation_time, people } =
+  const { reservation_id, first_name, last_name, mobile_number, reservation_date, reservation_time, people } =
     props.reservation;
   return <tr>
+    <td>{reservation_id}</td>
     <td>{first_name}</td>
     <td>{last_name}</td>
     <td>{reservation_date}</td>
     <td>{reservation_time}</td>
     <td>{people}</td>
     <td>{mobile_number}</td>
+    <td><a className="btn btn-outline-primary" href={`/reservations/${reservation_id}/seat`}>Seat</a></td>
   </tr>
 };
 
--- front-end/src/dashboard/ReservationsTable.js
+++ front-end/src/dashboard/ReservationsTable.js
@@ -6,17 +6,19 @@ const ReservationsTable = ({ reservations }) => {
     <table className="table">
       <thead>
         <tr>
+          <th>ID</th>
           <th>First Name</th>
           <th>Last Name</th>
           <th>Date</th>
           <th>Time</th>
           <th>People</th>
           <th>Mobile #</th>
+          <th>Seat</th>
         </tr>
       </thead>
       <tbody>
         {reservations.map((reservation, index) => (
-          <ReservationRecord key={index} reservation={reservation} />
+          <ReservationRecord key={reservation.reservation_id} reservation={reservation} />
         ))}
       </tbody>
     </table>
