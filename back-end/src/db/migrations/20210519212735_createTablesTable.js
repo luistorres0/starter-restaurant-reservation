f7d379c2eff98883b3a677864c178be78ad7d482
Luis Torres <luisdtor@gmail.com>
2021-05-26 09:51:20 -0500

    Made 'table_name' and 'capacity' fields in 'tables' table to be not nullable

--- back-end/src/db/migrations/20210519212735_createTablesTable.js
+++ back-end/src/db/migrations/20210519212735_createTablesTable.js
@@ -1,8 +1,8 @@
 exports.up = function (knex) {
   return knex.schema.createTable("tables", (table) => {
     table.increments("table_id").primary();
-    table.string("table_name");
-    table.integer("capacity");
+    table.string("table_name").notNullable();
+    table.integer("capacity").notNullable();
     table.integer("reservation_id").unsigned();
     table.foreign("reservation_id").references("reservation_id").inTable("reservations");
     table.timestamps(true, true);
